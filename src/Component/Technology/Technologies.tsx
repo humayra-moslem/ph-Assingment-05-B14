cb70634dd5389b51e774d3abdec4f7ececbd7e03
humayra-moslem <humayramoslem@gmail.com>
2026-09-13 19:27:06 +0600

    update stack to funtionally

--- src/Component/Technology/Technologies.tsx
+++ src/Component/Technology/Technologies.tsx
@@ -1,114 +1,3 @@
-// import React, { use, useState } from "react";
-// import TechnologyCard from "./TechnologyCard";
-// import Stack from "./Stack";
-// import { toast } from "react-toastify";
-// import type { Itechtype } from "../types/techtype";
-// interface techProps {
-//   techpromise: Promise<Itechtype[]>;
-// }
-
-// const Technologies = ({ techpromise }: techProps) => {
-//   const technologies = use(techpromise);
-//   // console.log(techpromise);
-//   const [stack, setStack] = useState<Itechtype[]>([]);
-
-//   // Add technology
-//   const handleAdd = (technology: Itechtype) => {
-//     setStack((previousStack) => {
-//       // Prevent duplicate
-//       const alreadyExists = previousStack.some(
-//         (item) => item.id === technology.id,
-//       );
-
-//       if (alreadyExists) {
-        
-//         toast.warning(`${technology.name} is already in your stack!`);
-//           return previousStack;
-//       }
-//       toast.success(`${technology.name} added to your stack!`);
-//       return [...previousStack, technology];
-//     });
-//   };
-
- 
-//   const handleRemove = (id: string) => {
-//   setStack((previousStack) => {
-//     const removedTechnology = previousStack.find(
-//       (item) => item.id === id
-//     );
-
-//     if (removedTechnology) {
-//       toast.info(`${removedTechnology.name} removed from your stack.`);
-//     }
-
-//     return previousStack.filter(
-//       (item) => item.id !== id
-//     );
-//   });
-// };
-
- 
-//   const handleRemoveAll = () => {
-//   if (stack.length === 0) {
-//     toast.warning("Your stack is already empty!");
-//     return;
-//   }
-
-//   setStack([]);
-
-//   toast.success("All technologies removed!");
-// };
-
-//   return (
-//     <div>
-//       <section className="max-w-6xl mx-auto px-4 py-10">
-        
-     
-//       <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-
-//         {/* LEFT: Technology Cards */}
-//         <div className="lg:col-span-3">
-
-//           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-
-            
-//             {technologies.map((technology) => {
-//     const isAdded = stack.some(
-//       (item) => item.id === technology.id
-//     );
-
-//     return (
-//       <TechnologyCard
-//         key={technology.id}
-//         technology={technology}
-//         onAdd={handleAdd}
-//         isAdded={isAdded}
-        
-//       />
-//     );
-//   })}
-
-//           </div>
-
-//         </div>
-
-//         {/* RIGHT: Stack */}
-//         <div className="lg:col-span-1">
-//           <Stack
-//             stack={stack}
-//             onRemove={handleRemove}
-//             onRemoveAll={handleRemoveAll}
-//           />
-//         </div>
-
-//       </div>
-
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Technologies;
 
 import { use, useState } from "react";
 import { toast } from "react-toastify";
@@ -127,9 +16,7 @@ const Technologies = ({ techpromise }: TechProps) => {
 
   const [stack, setStack] = useState<Itechtype[]>([]);
 
-  // =========================
-  // ADD TO STACK
-  // =========================
+  
   const handleAdd = (technology: Itechtype) => {
     // Check duplicate using current stack
     const alreadyExists = stack.some(
@@ -155,9 +42,7 @@ const Technologies = ({ techpromise }: TechProps) => {
     );
   };
 
-  // =========================
-  // REMOVE ONE
-  // =========================
+  
   const handleRemove = (id: string) => {
     const removedTechnology = stack.find(
       (item) => item.id === id
@@ -178,9 +63,6 @@ const Technologies = ({ techpromise }: TechProps) => {
     );
   };
 
-  // =========================
-  // REMOVE ALL
-  // =========================
   const handleRemoveAll = () => {
     if (stack.length === 0) {
       toast.warning(
